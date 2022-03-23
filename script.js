895c4f5102f0438ddc3a1cdce2a263c8ef0f4a46
LuisaFValderramaO <valderramaluisa448@gmail.com>
2022-03-22 20:24:49 -0500

    DIPLOMADO

---
+++ script.js
@@ -0,0 +1,72 @@
+$(".pais1").css("color","purple")
+
+$(".pais1").click( function(){
+
+    $(".pais1").css("font-size","70px").css("color","green")
+})
+
+$(".pais2").click( function(){
+
+    $(".pais2").css("font-size","60px").css("color","blue")
+})
+
+$(".pais3").click( function(){
+
+    $(".pais3").css("background","pink")
+})
+$(".pais4").css("color","pink")
+
+$(".pais4").click( function(){
+
+    $(".pais4").css("font-size","40px").css("color","orange")
+})
+$(".pais5").css("color","purple")
+
+$(".pais5").click( function(){
+
+    $(".pais5").css("font-size","70px").css("color","green")
+})
+$(".pais6").css("color","purple")
+
+$(".pais6").click( function(){
+
+    $(".pais6").css("font-size","80px").css("color","violet")
+})
+$(".pais7").css("color","purple")
+
+$(".pais7").click( function(){
+
+    $(".pais7").css("font-size","100px").css("color","brown")
+})
+$(".pais8").css("color","purple")
+
+$(".pais8").click( function(){
+
+    $(".pais8").css("font-size","50px").css("color","gray")
+})
+
+$(".evento2").dblclick( function(){
+
+    $(".evento2").css("font-size","60px").css("color","yellow")
+})
+
+$(".evento3").dblclick( function(){
+
+    $(".evento3").css("font-size","60px").css("color","blue")
+})
+
+$(".evento4").dblclick( function(){
+
+    $(".evento4").css("font-size","60px").css("color","red")
+})
+
+
+$(".caja1").mouseover( function(){
+
+    $(".caja1").css("background","yellow")
+})
+
+$(".").mouseout( function(){
+
+    $(".caja1").css("background","purple")
+})
